5f786a48181469189c209aa1c802ebde49c13631
Valeria Kliui <valeria14003@gmail.com>
2023-07-27 22:22:27 +0300

    started refactoring

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { useEffect } from 'react';
+import { useAppDispatch } from '../src/assets/hooks/hooks';
+import { Route, Routes } from 'react-router-dom';
+import './assets/scss/app.scss';
+import Header from './components/Header';
+import { Footer } from './components/Footer';
+import { Home } from './pages/Home';
+import { Cart } from './pages/Cart';
+import { Item } from './pages/Item';
+
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <div></div>
+    </>
+  );
+};
+
+export default App;
